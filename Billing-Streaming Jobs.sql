a5eafc799edd938c5e15ec409ff20bde633c184f
azilbergerts <43246200+azilbergerts@users.noreply.github.com>
2025-01-15 12:31:08 -0500

    Update Billing-Streaming Jobs.sql
    Update query to display data grouped by year/month

--- Billing-Streaming Jobs.sql
+++ Billing-Streaming Jobs.sql
@@ -1,15 +1,21 @@
+-- STREAMING JOBS - GA4 Intraday tables
 SELECT
   -- start_timestamp,
   -- error_code,
+EXTRACT(MONTH from DATETIME(start_timestamp)) month, 
+EXTRACT(YEAR from DATETIME(start_timestamp)) year, 
+dataset_id,
+table_id,
+SUM(total_rows) total_rows,
+SUM(total_input_bytes) total_input_bytes
   -- SUM(total_requests) AS num_failed_requests
-  *
+ 
 FROM
   `region-us`.INFORMATION_SCHEMA.STREAMING_TIMELINE_BY_PROJECT
-WHERE date(start_timestamp) ='2023-10-03'
-  and error_code IS NOT NULL
+WHERE date(start_timestamp) >='2024-10-03'
+  -- and error_code IS NOT NULL
   -- AND start_timestamp > TIMESTAMP_SUB(CURRENT_TIMESTAMP, INTERVAL 30 MINUTE)
--- GROUP BY
+GROUP BY ALL
 --   start_timestamp,
 --   error_code
-ORDER BY
-  start_timestamp DESC;
+ORDER BY month, year
